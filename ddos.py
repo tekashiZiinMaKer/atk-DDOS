0221accb8fbda1250ee07ab6230816cea5225357
tekashiZiinMaKer <98004421+tekashiZiinMaKer@users.noreply.github.com>
2022-02-24 03:25:10 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -20,7 +20,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print(' Author   : tekashiZiinMaKer)
+print(' Author   : tekashiZiinMaKer')
 print(' github   : https://github.com/tekashiZiinMaKer')
 print
 ip = int(input(' IP Target : '))
